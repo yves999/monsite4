44f657e44efa9b3ef7e5d7a287ec801ea7f0482d
yves2222 <lohier.ginette@free.fr>
2026-06-11 10:12:25 +0200

    initialisation database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,7 @@ import affichageRoutes from "./routes/affichage.routes";
 import affichageJtRoutes from "./routes/affichage-jt.routes";
 import affichageJt2Routes from "./routes/affichage-jt2.routes";
 import gestionJtRoutes from "./routes/gestion-jt.routes";
+import { pool } from "./database/db";
 
 const app = express();
 
@@ -38,6 +39,49 @@ app.use("/jt", jtRoutes);
 // app.listen(3000, () => {
 //     console.log("Backend lancé sur http://localhost:3000");
 // });
+
+
+
+app.get("/init-db", async (req, res) => {
+try {
+        await pool.query(`
+            CREATE TABLE jt (
+                id SERIAL PRIMARY KEY,
+                nom VARCHAR(100),
+                prenom VARCHAR(100),
+                mel VARCHAR(100),
+                rog VARCHAR(100),
+                wim VARCHAR(100),
+                uso VARCHAR(100)
+            );
+        `);
+
+        await pool.query(`
+            INSERT INTO jt (id, nom, prenom, mel, rog, wim, uso)
+            VALUES
+            (3, 'Djokovic', 'Novak', '10', '3', '7', '4'),
+            (1, 'Federer', 'Roger', '6', '1', '8', '5'),
+            (2, 'Nadal', 'Rafael', '2', '14', '2', '4'),
+            (97, 'Agassi', 'André', '4', '1', '1', '2'),
+            (98, 'Sampras', 'Pete', '2', '0', '7', '5'),
+            (99, 'Connors', 'Jimmy', '1', '0', '2', '5'),
+            (100, 'Mac Enroe', 'John', 'demi-finaliste', 'finaliste', '3', '4'),
+            (101, 'Borg', 'Björn', '0', '6', '5', 'finaliste')  
+        `);
+
+        res.send("Base initialisée");
+
+} 
+
+
+
+catch (err) {
+console.error(err);
+res.status(500).send("Erreur");
+}
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 
