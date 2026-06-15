c192e53bb8b70783750a49874f468a8471af7caa
yves2222 <lohier.ginette@free.fr>
2026-06-15 18:13:06 +0200

    Ajout test-jt

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -89,43 +89,47 @@ app.use("/jt", jtRoutes);
 // });
 
 
-
-app.get("/init-db", async (req, res) => {
-try {
-        await pool.query(`
-            CREATE TABLE jt (
-                id SERIAL PRIMARY KEY,
-                nom VARCHAR(100),
-                prenom VARCHAR(100),
-                mel VARCHAR(100),
-                rog VARCHAR(100),
-                wim VARCHAR(100),
-                uso VARCHAR(100)
-            );
-        `);
-
-        await pool.query(`
-            INSERT INTO jt (id, nom, prenom, mel, rog, wim, uso)
-            VALUES
-            (3, 'Djokovic', 'Novak', '10', '3', '7', '4'),
-            (1, 'Federer', 'Roger', '6', '1', '8', '5'),
-            (2, 'Nadal', 'Rafael', '2', '14', '2', '4'),
-            (97, 'Agassi', 'André', '4', '1', '1', '2'),
-            (98, 'Sampras', 'Pete', '2', '0', '7', '5'),
-            (99, 'Connors', 'Jimmy', '1', '0', '2', '5'),
-            (100, 'Mac Enroe', 'John', 'demi-finaliste', 'finaliste', '3', '4'),
-            (101, 'Borg', 'Björn', '0', '6', '5', 'finaliste')  
-        `);
-
-        res.send("Base initialisée");
-
-} 
-
-catch (err) {
-console.error(err);
-res.status(500).send("Erreur");
-}
+app.get("/test123", (req, res) => {
+  res.send("TEST OK");
 });
+
+
+// app.get("/init-db", async (req, res) => {
+// try {
+//         await pool.query(`
+//             CREATE TABLE jt (
+//                 id SERIAL PRIMARY KEY,
+//                 nom VARCHAR(100),
+//                 prenom VARCHAR(100),
+//                 mel VARCHAR(100),
+//                 rog VARCHAR(100),
+//                 wim VARCHAR(100),
+//                 uso VARCHAR(100)
+//             );
+//         `);
+
+//         await pool.query(`
+//             INSERT INTO jt (id, nom, prenom, mel, rog, wim, uso)
+//             VALUES
+//             (3, 'Djokovic', 'Novak', '10', '3', '7', '4'),
+//             (1, 'Federer', 'Roger', '6', '1', '8', '5'),
+//             (2, 'Nadal', 'Rafael', '2', '14', '2', '4'),
+//             (97, 'Agassi', 'André', '4', '1', '1', '2'),
+//             (98, 'Sampras', 'Pete', '2', '0', '7', '5'),
+//             (99, 'Connors', 'Jimmy', '1', '0', '2', '5'),
+//             (100, 'Mac Enroe', 'John', 'demi-finaliste', 'finaliste', '3', '4'),
+//             (101, 'Borg', 'Björn', '0', '6', '5', 'finaliste')  
+//         `);
+
+//         res.send("Base initialisée");
+
+// } 
+
+// catch (err) {
+// console.error(err);
+// res.status(500).send("Erreur");
+// }
+// });
 
 
 
