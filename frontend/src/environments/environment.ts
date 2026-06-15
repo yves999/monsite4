168d69e52a7de83bada2fac9b17b90e1a9ae3e79
yves2222 <lohier.ginette@free.fr>
2026-06-15 23:57:56 +0200

    Ajout test-jt

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,22 @@ export const environment = {
   production: false,
   apiUrl:
     window.location.hostname === 'localhost'
-      ? 'http://localhost:3000'
+      ? 'https://monsite4-3.onrender.com'
       : 'https://monsite4-3.onrender.com'
 };
+
+
+
+
+
+
+
+
+// export const environment = {
+//   production: false,
+//   apiUrl:
+//     window.location.hostname === 'localhost'
+//       ? 'http://localhost:3000'
+//       : 'https://monsite4-3.onrender.com'
+// };
 
